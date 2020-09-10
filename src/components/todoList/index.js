34f6ee80a7dc010fac010d3123499237c3f14401
Bishalsarang <bishalsarang@gmail.com>
2020-09-10 18:29:49 +0545

    Use repeat directive instead of map

--- src/components/todoList/index.js
+++ src/components/todoList/index.js
@@ -4,6 +4,9 @@ import {
   render
 } from '../../../node_modules/lit-html';
 
+import {repeat} from '../../../node_modules/lit-html/directives/repeat.js';
+
+
 import {
   PRIORITIES
 } from '../../constants/index.js';
@@ -23,16 +26,7 @@ class TodoList extends HTMLElement {
    * Runs when component is added.
    */
   connectedCallback() {
-    this.listTemplate = (todoList) => {
-      return (
-        html `
-    <add-form></add-form>
     
-    <ul class='todo-list'>
-      ${todoList.map(({id, title, priority, isComplete}) => html`<todo-item id=${id} title=${title} isComplete=${isComplete} priority=${priority}  @onDeleteTodo=${this.deleteTodo.bind(this)}  @onToggleTodo=${this.toggleTodo.bind(this)} @onToggleTodoPriority=${this.togglePriority.bind(this)}></todo-item>`)}
-    </ul>
-    `)
-    };
 
     // Load state from local storage
     this.getLocalStorage();
@@ -63,6 +57,17 @@ class TodoList extends HTMLElement {
     localStorage.setItem('todos', JSON.stringify(this.todos));
   }
 
+  listTemplate(){
+    return (
+      html `
+  <add-form></add-form>
+  
+  <ul class='todo-list'>
+    ${repeat(this.todos, (todo) => todo.id, ({id, title, isComplete, priority}) =>  html`<todo-item id=${id} title=${title} isComplete=${isComplete} priority=${priority}  @onDeleteTodo=${this.deleteTodo.bind(this)}  @onToggleTodo=${this.toggleTodo.bind(this)} @onToggleTodoPriority=${this.togglePriority.bind(this)}></todo-item>`)}
+    </ul>
+  `)
+  }
+
   addTodo(e) {
     const {
       body: newToDo
@@ -75,14 +80,15 @@ class TodoList extends HTMLElement {
   }
 
   /**
-   * Receive id in custom event object and return new todo list after removing the id
+   * Receive id in custom event object and return new todo list after removing the id. 
+   * 
    * @param {Object} e Event Object
    */
   deleteTodo(e) {
     const id = e.detail.id;
-console.log("before",this.todos)
+
     this.todos = this.todos.filter((item) => item.id !== id);
-console.log("aftwr",this.todos)
+
     this.updateLocalStorage();
     this.render();
   }
@@ -129,8 +135,7 @@ console.log("aftwr",this.todos)
   }
 
   render() {
-    console.log(this.todos)
-    render(this.listTemplate(this.todos),this.root, {eventContext: this});
+    render(this.listTemplate(),this.root);
  
   }
 }
